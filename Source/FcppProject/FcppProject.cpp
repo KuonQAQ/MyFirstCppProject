37e9fb0ad0d42077cbeda7efbd7edb2ac0810ea5
KuonQAQ <liyuangoole2023@gmail.com>
2023-10-31 12:34:44 +0800

    Init

---
+++ Source/FcppProject/FcppProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FcppProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FcppProject, "FcppProject" );
---
+++ Source/FcppProject/FcppProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FcppProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FCPPPROJECT_API AFcppProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
